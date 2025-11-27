e36ac7870a911278a418b7d6230152485a24f258
Brandon256star <U20201E762@upc.edu.pe>
2025-11-27 05:21:31 +0000

    He creado unidad-module

---
+++ src/app/models/unidad-model.ts
@@ -0,0 +1,15 @@
+export interface unidad
+{
+      id: number,
+      titulo: String,
+      descripcion: String,
+      nivel: number,
+      categoria: String,
+      duracion: number
+}
+
+
+
+
+
+
