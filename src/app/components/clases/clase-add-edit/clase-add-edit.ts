c2ba062b00f308f8c732f9e46cc8dbc3d1b7f976
fernando <fernando.meneses0712@gmail.com>
2025-11-27 20:02:09 -0500

    avance clases component

--- src/app/components/clases/clase-add-edit/clase-add-edit.ts
+++ src/app/components/clases/clase-add-edit/clase-add-edit.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { ClaseService } from '../../../services/clase-service';
+import { UnidadService } from '../../../services/unidad-service';
+import { unidad } from '../../../models/unidad-model';
+import { clase } from '../../../models/clase-model';
+import { FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-clase-add-edit',
@@ -8,4 +16,42 @@ import { Component } from '@angular/core';
 })
 export class ClaseAddEdit {
 
+crudForm!:FormGroup;
+idClase:number = 0;
+unidad!:unidad[];
+
+constructor(private snack:MatSnackBar,private router:Router,private activatedRoute:ActivatedRoute,
+  private claseService:ClaseService, private unidadService:UnidadService){}
+
+
+
+  ngOnInit(){
+    this.CargarFormulario();
+  }
+
+
+  CargarFormulario(){
+
+
+  }
+
+
+  /*grabar (){
+
+    const clase:clase = {
+
+      idClase:this.crudForm.get("id")?.value,
+
+
+
+    }
+
+  }*/
+
+
+  CargarClases(){
+
+
+
+  }
 }
